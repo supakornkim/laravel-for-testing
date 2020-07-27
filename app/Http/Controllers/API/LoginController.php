74004fbea79a3dc841d13aac909745ec6d2158d6
Supakorn Kimhajan <supakornkimhajan@Supakorns-MacBook-Pro.local>
2020-07-27 11:25:43 +0700

    first commit

---
+++ app/Http/Controllers/API/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\APIBaseController as APIBaseController;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use DB;
+
+class LoginController extends APIBaseController
+{
+    public function login(){
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+            $user = Auth::user();
+            return $this->sendResponse($user, 'User login successfully.');
+        }
+        else{
+            return $this->sendError('Login Fail!.');
+        }
+    }
+}
